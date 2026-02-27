d3b48d04644ddb299c9a47766c63c79ca129be6f
SOGITEST <sohatvs42@gmail.com>
2026-02-27 15:32:59 +0800

    Added logout button to clear session

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -11,21 +11,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/login", "/css/**").permitAll() // Benarkan akses ke page tertentu
-                        .anyRequest().authenticated() // Page lain SEMUA kena login
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login") // Beritahu URL ke page login
-                        .defaultSuccessUrl("/dashboard", true) // Jika login berjaya, pergi ke dashboard
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
-
+            .authorizeHttpRequests((requests) -> requests
+                    .requestMatchers("/", "/login", "/css/**").permitAll() // Benarkan akses ke page tertentu
+                    .anyRequest().authenticated() // Page lain SEMUA kena login
+            )
+            .formLogin((form) -> form
+                    .loginPage("/login") // Beritahu URL ke page login
+                    .defaultSuccessUrl("/dashboard", true) // Jika login berjaya, pergi ke dashboard
+                    .permitAll()
+            )
+            .logout((logout) -> logout
+                    .logoutUrl("/logout") // URL untuk trigger logout
+                    .logoutSuccessUrl("/login?logout") // Lepas keluar, pergi login
+                    .invalidateHttpSession(true) // Padam session dalam server
+                    .deleteCookies("JSESSIONID") // Padam kunci dalam browser
+                    .permitAll());
         return http.build();
     }
 
